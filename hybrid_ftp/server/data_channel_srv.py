93ab3f41d5addacd2dc244c24d8c6aba97fa3036
NamQuanProject <namquanproject@gmail.com>
2026-07-25 20:59:17 +0700

    Adding the data channel of srv

---
+++ hybrid_ftp/server/data_channel_srv.py
@@ -0,0 +1,24 @@
+import socket
+from ..common.rdt_socket import ReliableUDPSocket
+
+
+def open_passive(session):
+    rdt_sock = ReliableUDPSocket()
+    rdt_sock.bind(("0.0.0.0", 0))
+    session.data_socket = rdt_sock
+    session.data_mode = "PASSIVE"
+    addr = rdt_sock.sock.getsockname()
+    ip = session.server_ip_for_client
+    if ip == "0.0.0.0" or ip.startswith("127."):
+        ip = "127.0.0.1"
+    return (ip, addr[1])
+
+
+def open_active(session, ip, port):
+    rdt_sock = ReliableUDPSocket()
+    rdt_sock.bind(("0.0.0.0", 0))
+    peer = (ip, port)
+    session.data_socket = rdt_sock
+    session.data_mode = "ACTIVE"
+    session.peer_addr = peer
+    return rdt_sock
